e1644cab1a52f0bea53d64ae4808e6a13f62a20c
NadiaTurko <47818620+NadiaTurko@users.noreply.github.com>
2023-06-14 11:33:03 +0300

    Update app.js

--- app.js
+++ app.js
@@ -75,6 +75,30 @@ console.log(mul(1, "str", 2, 3, true)); // 6
 //       }).bind(this);
 //    }
 // };
+let server = {
+  data: 0,
+  convertToString: function (callback) {
+    callback(() => {
+      return this.data + "";
+    });
+  },
+};
+let client = {
+  server: server,
+  result: "",
+  calc: function (data) {
+    this.server.data = data;
+    this.server.convertToString(this.notification());
+  },
+  notification: function () {
+    return (callback) => {
+      this.result = callback();
+    };
+  },
+};
+client.calc(123);
+console.log(client.result); // "123"
+console.log(typeof client.result); // "string"
 // client.calc(123);
 // console.log(client.result); // "123"
 // console.log(typeof client.result); // "string"
