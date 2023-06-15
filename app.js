15cf9e74537cd23f4c993fd22580f5bb98161b66
NadiaTurko <47818620+NadiaTurko@users.noreply.github.com>
2023-06-15 12:59:45 +0300

    Update app.js

--- app.js
+++ app.js
@@ -135,6 +135,12 @@ const mapBuilder = (keysArray, valuesArrays) => {
 //       console.log(i);
 //    };
 // }
+const arr = [];
 
+for (let i = 0; i <= 2; i++) {
+  arr[i] = function () {
+    console.log(i);
+  };
+}
 // arr[0](); // 0
 // arr[arr.length - 1](); // 2
